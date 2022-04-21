2d264484e83d30eaa9b5a353ca17ec0c4426c71e
Alex <alejandro.rod.lop@gmail.com>
2022-04-21 19:43:43 +0200

    Added final JDBC files

--- 8/Proyecto-SesionJDBC/src/SesionJDBC.java
+++ 8/Proyecto-SesionJDBC/src/SesionJDBC.java
@@ -19,10 +19,14 @@ public class SesionJDBC {
 	public static void main(String[] args) {
 		try {
 			ejemplo1();
-			ejemplo2();
+			//ejemplo2();
 			
 			//Sigue incorporando mas ejemplos			
-			
+			ejemplo3();
+			//ejemplo4();
+			ejemplo5();
+			ejemplo6();
+			prueba();
 			
 		} catch (SQLException e) { //Tratamiento por Excepcion 
 			
@@ -54,6 +58,7 @@ public class SesionJDBC {
 		st.close();
 		con.close();
 	}
+
 	/*
 	Ejemplo 2. Realizar un metodo en Java que presente por pantalla 
 	           toda la informacion de un profesor, se pedirá su nombre por entrada estandard
@@ -71,7 +76,7 @@ public class SesionJDBC {
 		
 		//Crear el objeto Statement para realizar las sentencias de la base de datos
 		PreparedStatement st = con.prepareStatement(query.toString());
-		
+	
 		System.out.println("Introduce el nombre del profesor: ");
 	    String nombre = LeerString();
 	    st.setString(1, nombre);
@@ -85,6 +90,154 @@ public class SesionJDBC {
 		con.close();
 	}
 	
+	/**
+	 * Ejemplo 3
+	 * 
+	 * @throws SQLException
+	 */
+	private static void ejemplo3() throws SQLException {
+		System.out.println("################### EJEMPLO 3 ###################");
+		Connection con = getConexion ();
+		StringBuilder query = new StringBuilder ();
+		query.append("SELECT g1.grado_nombre "
+				+ "FROM grado g1 "
+				+ "WHERE NOT EXISTS ("
+				+ "SELECT DISTINCT g2.grado_nombre "
+				+ "FROM grado g2 "
+				+ "INNER JOIN grado_modulo gm USING(grado_id) "
+				+ "INNER JOIN modulo m USING (modulo_id) "
+				+ "WHERE m.modulo_nombre='Fundamentos de Informatica' "
+				+ "AND g1.grado_id = g2.grado_id "
+				+ ")");
+		PreparedStatement st = con.prepareStatement(query.toString());
+		ResultSet rs = st.executeQuery();
+		presentaResultados(rs);
+		
+		rs.close();
+		st.close();
+		con.close();
+	}
+
+	/**
+	 * Ejemplo 4
+	 * 
+	 * @throws SQLException
+	 */
+	private static void ejemplo4() throws SQLException {
+		System.out.println("################### EJEMPLO 4 ###################");
+		Connection con = getConexion ();
+		StringBuilder query = new StringBuilder ();
+		query.append("SELECT g1.grado_nombre "
+				+ "FROM grado g1 "
+				+ "WHERE NOT EXISTS ( "
+				+ "SELECT DISTINCT g2.grado_nombre "
+				+ "FROM grado g2 "
+				+ "INNER JOIN grado_modulo gm USING(grado_id) "
+				+ "INNER JOIN modulo m USING (modulo_id) "
+				+ "WHERE m.modulo_nombre = ? "
+				+ "AND g1.grado_id = g2.grado_id "
+				+ ")");
+		
+		
+		PreparedStatement st = con.prepareStatement(query.toString());
+		System.out.println("Introduce el nombre de la asignatura: ");
+	    st.setString(1, LeerString());
+
+		ResultSet rs = st.executeQuery();
+		presentaResultados(rs);
+		
+		rs.close();
+		st.close();
+		con.close();
+	}
+
+	/**
+	 * Ejemplo 5
+	 * 
+	 * @throws SQLException
+	 */
+	private static void ejemplo5() throws SQLException {
+		System.out.println("################### EJEMPLO 5 ###################");
+		Connection con = getConexion ();
+		StringBuilder query = new StringBuilder ();
+		query.append("SELECT DISTINCT estudiante_nombre, estudiante_apellidos "
+				+ "FROM estudiante est "
+				+ "INNER JOIN estudiante_grado_modulo egm USING (estudiante_id) "
+				+ "INNER JOIN grado g USING (grado_id) "
+				+ "WHERE LOWER (g.grado_nombre) = 'ingenieria informatica en tecnologias de la informacion' "
+				+ "AND estudiante_id NOT IN ( "
+				+ "SELECT estudiante_id "
+				+ "FROM estudiante est "
+				+ "INNER JOIN estudiante_grado_modulo egm USING (estudiante_id) "
+				+ "INNER JOIN modulo m USING (modulo_id) "
+				+ "WHERE LOWER (modulo_nombre) = 'estructuras de datos')");
+	
+		
+		PreparedStatement st = con.prepareStatement(query.toString());
+		ResultSet rs = st.executeQuery();
+		presentaResultados(rs);
+		
+		rs.close();
+		st.close();
+		con.close();
+	}
+
+	/**
+	 * Ejemplo 6
+	 * 
+	 * @throws SQLException
+	 */
+	private static void ejemplo6() throws SQLException {
+		System.out.println("################### EJEMPLO 6 ###################");
+		Connection con = getConexion ();
+		StringBuilder query = new StringBuilder ();
+		query.append("SELECT DISTINCT estudiante_nombre, estudiante_apellidos "
+				+ "FROM estudiante est "
+				+ "INNER JOIN estudiante_grado_modulo egm USING (estudiante_id) "
+				+ "INNER JOIN grado g USING (grado_id) "
+				+ "WHERE LOWER (g.grado_nombre) = LOWER(?) "
+				+ "AND estudiante_id NOT IN ( "
+				+ "SELECT estudiante_id "
+				+ "FROM estudiante est "
+				+ "INNER JOIN estudiante_grado_modulo egm USING (estudiante_id) "
+				+ "INNER JOIN modulo m USING (modulo_id) "
+				+ "WHERE LOWER (modulo_nombre) = LOWER(?))");
+	
+		
+		PreparedStatement st = con.prepareStatement(query.toString());
+		System.out.println("Introduce el nombre del grado: ");
+	    st.setString(1, LeerString());
+		System.out.println("Introduce el nombre del modulo: ");
+	    st.setString(2, LeerString());
+		ResultSet rs = st.executeQuery();
+		presentaResultados(rs);
+		
+		rs.close();
+		st.close();
+		con.close();
+	}
+
+	private static void prueba () throws SQLException {
+		System.out.println("################### EJEMPLO 7 ###################");
+		Connection con = getConexion();
+		
+		StringBuilder query = new StringBuilder ();
+		
+		query.append("UPDATE estudiante SET estudiante_apellidos='Palacio Gomez Prueba' "
+				+ "WHERE estudiante_id = ?");
+		PreparedStatement st = con.prepareStatement(query.toString());
+		System.out.println("Introduce el ID del estudiante: ");
+		st.setInt(1, LeerInt());
+
+		int rs = st.executeUpdate();
+		System.out.printf("Se han insertado %d tuplas\n", rs);
+		//presentaResultados(rs);
+		
+		//rs.close();
+		st.close();
+		con.close();
+	}
+
 	private static Connection getConexion() throws SQLException{
 		
 		return  DriverManager.getConnection(STRING_CONEXION,USUARIO,PASSWORD);
